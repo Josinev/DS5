d7096395406924f5176abfb941c68c6ca29527b8
MilouSloendregt <m.sloendregt@student.fontys.nl>
2023-09-20 12:18:59 +0200

    lalala

--- opdracht_2.py
+++ opdracht_2.py
@@ -1,25 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-width = 200
-height = 200
-x_min, x_max = -1.5, 0.5
-y_min, y_max = -1, 1
-max_iterations = 100
 
-mandelbrot_set = np.zeros((width, height), dtype=np.int32)
+def draw_mandel(width):
+    height = width
+    x_min, x_max = -1.5, 0.5
+    y_min, y_max = -1, 1
+    max_iterations = 100
+    mandelbrot_set = np.zeros((width, height))
+    for i in range(width):
+        for j in range(height):
+            x = x_min + (x_max - x_min) * i / (width - 1)
+            y = y_min + (y_max - y_min) * j / (height - 1)
+            c = complex(x,y)
+        
 
-for i in range(width):
-    for j in range(height):
-        x = x_min + (x_max - x_min) * i / (width - 1)
-        y = y_min + (y_max - y_min) * j / (height - 1)
-        c = complex(x,y)
+            z = 0
+            for n in range(max_iterations):
+                if abs(z) > 2:
+                    mandelbrot_set[i, j] = n
+                    break
+                z = z * z + c
 
-        z = 0
-        for n in range(max_iterations):
-            if abs(z) > 2:
-                mandelbrot_set[i, j] = n
-                break
-            z = z * z + c
-
-plt.imshow(mandelbrot_set, extent=(x_min, x_max, y_min, y_max))
-plt.show()
+    plt.imshow(mandelbrot_set)
+    plt.show()
+            
+draw_mandel(200)
---
+++ opdracht_3.py
@@ -0,0 +1,5 @@
+import networkx as net
+import matplotlib.pyplot as plt
+
+star = net.star_graph(5)
+plt.show()
