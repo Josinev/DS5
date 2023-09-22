5f3558464c807317c795583ad5636dbf37fa6af7
MilouSloendregt <m.sloendregt@student.fontys.nl>
2023-09-22 09:00:21 +0200

    test

--- opdracht_3.py
+++ opdracht_3.py
@@ -1,5 +1,16 @@
-import networkx as net
+import numpy as np
+import networkx as nx
+import numpy as np
 import matplotlib.pyplot as plt
 
-star = net.star_graph(5)
-plt.show()
+
+def create_initial_star_graph(n0):
+    G = nx.Graph()
+    G.add_node(0)  # Hub node
+    for i in range(1, n0):
+        G.add_node(i)
+        G.add_edge(0, i)
+    print(G)
+n0 = 5  # You can change this value
+initial_graph = create_initial_star_graph(n0)
+
