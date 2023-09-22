d6d9370e63f0ec093efd790ff7a13617a7de51f5
MilouSloendregt <m.sloendregt@student.fontys.nl>
2023-09-22 09:24:29 +0200

    het is klaar

--- opdracht_2.py
+++ opdracht_2.py
@@ -1,26 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def draw_mandel(width):
-    height = width
-    x_min, x_max = -1.5, 0.5
-    y_min, y_max = -1, 1
-    max_iterations = 100
-    mandelbrot_set = np.zeros((width, height))
-    for i in range(width):
-        for j in range(height):
-            x = x_min + (x_max - x_min) * i / (width - 1)
-            y = y_min + (y_max - y_min) * j / (height - 1)
+def draw_mandel(lengte):
+    ''' Tekent een mandelbrot plaatje
+    input: lengte van de mandelbrot
+    output: plaatje mandelbrot'''
+    breedte = lengte
+    #minimum en maximum waardes van x en y
+    xmin= -1.5
+    xmax= 0.5
+    ymin= -1
+    ymax = 1
+    aantal_herhalingen = 100
+    #Maakt lege set met alleen maar nullen
+    mandelbrot_set = np.zeros((lengte, breedte))
+    
+    #Met deze forloop komer en 200 gelijke stapjes tussen xmin en xmax
+    #Hetzelfde geldt voor ymin en ymax
+    for i in range(lengte):
+        for j in range(breedte):
+            x = xmin + (xmax - xmin) * i / (lengte - 1)
+            y = ymin + (ymax - ymin) * j / (breedte - 1)
+            #Maakt complex getal
             c = complex(x,y)
         
-
+            #a_n = a_n-1 ** 2 + c
             z = 0
-            for n in range(max_iterations):
+            for n in range(aantal_herhalingen):
                 if abs(z) > 2:
                     mandelbrot_set[i, j] = n
                     break
-                z = z * z + c
+                z = z ** 2 + c
 
+    #Maakt daadwerkelijk het plaatje
     plt.imshow(mandelbrot_set)
     plt.show()
             
